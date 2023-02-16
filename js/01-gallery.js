2ef58e2e9ef9b05a13b5948486aebdfdac1eaf95
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-16 17:50:19 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,10 +9,45 @@ const items = [];
 
 galleryItems.forEach(image => {
     const galleryItem = document.createElement("div");
-    galleryItem.className = "gallery_item"
+    galleryItem.className = "gallery__item";
+    
+    const galleryLink = document.createElement("a");
+    galleryLink.className = "gallery__link";
+    galleryLink.href = image.original;
+
+    const galleryImage = document.createElement("img");
+    galleryImage.className = "gallery__image";
+    galleryImage.src = image.preview;
+    galleryImage.setAttribute("data-source", image.original);
+    galleryImage.alt = image.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+
+    items.push(galleryItem);
+
 });
- 
 
 
 
-  
+galleryImages.append(...items);
+
+
+galleryImages.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+       
+
+
+    const selectImage = event.target.getAttribute("data-source");
+
+    const instance = basicLightbox.create(`
+		<img src="${selectImage}" width="800" height="600" >
+	`);
+    
+    instance.show()
+
+});
+
