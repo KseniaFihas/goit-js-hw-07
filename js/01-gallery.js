bbc0e938d52115bb5799d78aafa34ae8738ca026
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-16 11:22:53 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImages = document.querySelector(".gallery");
+const items = [];
+
+
+galleryItems.forEach(image => {
+    const galleryItem = document.createElement("div");
+    galleryItem.className = "gallery_item"
+});
+ 
+
+
+
+  
