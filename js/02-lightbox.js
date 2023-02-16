b860a75ec7e18ea2eb04999e6eac5ecb4e2d668c
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-16 18:35:28 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImages = document.querySelector(".gallery");
+const items = [];
+
+
+galleryItems.forEach(image => {
+    
+    const galleryLink = document.createElement("a");
+    galleryLink.className = "gallery__link";
+    galleryLink.href = image.original;
+
+    const galleryImage = document.createElement("img");
+    galleryImage.className = "gallery__image";
+    galleryImage.src = image.preview;
+    galleryImage.setAttribute("title", image.description);
+    galleryImage.alt = image.description;
+
+    
+    galleryLink.append(galleryImage);
+
+    items.push(galleryLink);
+
+});
+
+galleryImages.append(...items);
+
+
+new SimpleLightbox('.gallery a', { captionDelay : 250});
+
